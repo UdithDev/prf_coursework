f2db5c6a51e4f84b424c38eb4b451dba18325da7
Udith_Dev_Wee <daveudith@gmail.com>
2023-06-27 14:21:43 +0530

    updated

--- MainClass.java
+++ MainClass.java
@@ -25,9 +25,18 @@ public class MainClass {
                 System.out.print("Password : ");
                 int password=scanner.nextInt();
                 isLogged=true;
+                homePage();
             }else{
                 System.out.println("user name is invalid. please try again!"); 
             }
         }
+    }
+
+    public static void homePage(){
+        System.out.println("+-----------------------------------------------+");
+        System.out.println("|       WELCOME TO IJSE STOCK MANAGMENT SYSTEM  |");
+        System.out.println("+-----------------------------------------------+");
+
+        System.out.println("[1] Change the credintials\t\t\t\t[2] Supplier Manage\n[3] Stock Manage\t\t\t\t\t[4] Log out\n[5] Exit the system");
     }
 }
